e86de766ccf689c1cc23c1ae7379c0d9097e0515
ClearanceC137 <clearancetshepiso@gmail.com>
2023-06-05 09:47:38 +0200

    Added Display functionality

--- src/Components/DisplayId/DisplayId.js
+++ src/Components/DisplayId/DisplayId.js
@@ -7,11 +7,13 @@
 */
 function DisplayId(props) {
     let Id = {IsValid:false,DateOfBirth:"", Gender:"", Citizenship:"", Race:0,checksum:"",IdNumber:""};
+    let id ="0010095594080";       // props.IdNumber
+    const DateOfBirth = new Date(parseInt(id.substr(0,2)) > 50 ? "19"+id.substr(0,2)+"-"+id.substr(2,2)+"-"+id.substr(4,2) :"20"+id.substr(0,2)+"-"+id.substr(2,2)+"-"+id.substr(4,2)); 
+    const Gender = parseInt(id.substr(6,4) < 5000) ? "Female" : "Male" ;  // 0000 < 4999 are female and 5000-9999
+    const Citizenship = parseInt(id.substr(10,1) === 0) ? "SA citizen" : "Permanent resident" ;  // SA citizen – 0 – or a permanent resident – 1
     return (
         <div>
            <text>Home Page</text><br/>
-           <input placeholder="Id"></input><br/>
-            <button >Validate</button>
         </div>
     );
 }
