66a36dca42c1ddaadfce71bf2147520f6a2f6be6
ClearanceC137 <clearancetshepiso@gmail.com>
2023-06-07 15:25:31 +0200

    luhn algorithm

--- src/Functions/ValidateId.js
+++ src/Functions/ValidateId.js
@@ -7,30 +7,28 @@ function ValidateId(Id) {
     /* -- set isValid false , value will only change if it passes the luhn algorithm -- */
     let IsValid = false;
     let RawId =Id.IdNumber;
-    /* -- validate the id using luhn algorithm  -- */
     let IdArray = RawId.split("");
     for(var i = 0 ; i < RawId.length ; i++){      //convert Id String into array elements(char)
         IdArray[i] =parseInt(RawId[i]); 
     }
     var len = IdArray.length;
-    /*
+    /*    Luhn algorithm
         Double alternate positioned numbers
     1. subtract 9 from the doubled number if greater than 9
     2. keep the double number the same , if less than 9
     */
-    for(var i=1; i<len ; i=i+2 ){
-        var value = [len-1-i]*2;
-        if( value>9 ){
-            IdArray[len-1-i] = value-9;
-        }
-        else{
-            IdArray[len-1-i] = value;
-        }
-    }
-    /* -- Sum up all the digits -- */
     var sum = 0;
-    for(var i=0 ; i<len ; i++ ){
-        sum+=IdArray[i];
+    var doubleNumber= false;
+    for (var i = IdArray.length - 1; i >= 0; i--) {
+        var num = IdArray[i];
+        if (doubleNumber) {
+        num =num*2;
+        if (num > 9) {
+            num -= 9;
+        }
+        }
+        sum += num;      // Sum the result of numbers
+        doubleNumber = !doubleNumber;
     }
     /* -- If the sum modulo 10 equals zero the Id is valid -- */
     if(sum%10 === 0){
