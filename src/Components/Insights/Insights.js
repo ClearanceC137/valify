5b0b55cf1ff06462c05f5168d3494f570f5177c4
ClearanceC137 <clearancetshepiso@gmail.com>
2023-06-09 10:03:19 +0200

    option for graphs and categories

--- src/Components/Insights/Insights.js
+++ src/Components/Insights/Insights.js
@@ -15,6 +15,8 @@ import "./Insights.css"
     3.Citizen/Resident
 */
 function Insights(){
+    const [Graph , setGraph] = useState([false,false,false]);
+    const [Category , setCategory] = useState([false,false,false]);
     const [Ids, setIds] = useState([]); //state for local cart array
     const [Render,setRender] =useState(true);
     const { userEmail, setUserEmail } = useContext(EmailContext);           //global state to be set to user after successful login
@@ -28,16 +30,47 @@ function Insights(){
     function ShowIds(){   //Renders all id inside the database
         return (
             <div>
-            {Ids.map((item, index) => (
-                <text key={index}>{DisplayId(item)}</text>
-            ))}
+                {Ids.map((item, index) => (
+                    <text key={index}>{DisplayId(item)}</text>
+                ))}
+                <div>
+                    <label for="graphs">Choose a graph to display:</label>
+                    <select id="graphs">
+                        <option id="0" onClick={onGraph}>Line graph</option>
+                        <option id="1" onClick={onGraph}>Bar graph</option>
+                        <option id="2" onClick={onGraph}>Pie Chart</option>
+                    </select>
+                    <br/>
+                    <label for="filter">Category</label>
+                    <select id="filter">
+                        <option id="0" onClick={onCategory}>Year of birth</option>
+                        <option id="1" onClick={onCategory}>Gender</option>
+                        <option id="2" onClick={onCategory}>CitizenShip</option>
+                    </select> 
+                </div>
             </div>
         );
     }
+    const onGraph = event => {      // Choose which type of graph to display
+        var temp = [false ,false , false];
+        let id = parseInt(event.target.id);
+        alert(id);
+        temp[id] = true;
+        setGraph(temp);
+    }
+    const onCategory = event => {      // choose to filter data by the choosen gategory
+        var temp = [false ,false , false];
+        let id = parseInt(event.target.id);
+        alert(id);
+        temp[id] = true;
+        setCategory(temp);
+    }
     const onDatabase = event => {         //Renders database
-        setRender(true);} 
+        setRender(true);
+    }
     const onInsights = event => {         //Render Insights
-        setRender(false);}
+        setRender(false);
+    }
     return (
         <div className='block'>
             <div className ="parent-bs">
