2ee401f6a627b2a8cd4cdf679dd8bc232a255512
ClearanceC137 <clearancetshepiso@gmail.com>
2023-06-12 00:06:08 +0200

    initialize id with default  with values

--- src/Functions/SaveId.js
+++ src/Functions/SaveId.js
@@ -11,7 +11,7 @@ function SavedId(email) {
 
     const addToDatabase = async () => {                            //handles adding an item to database
         await setDoc(doc(db, "IdMetaData", email), {
-            IsValid:false,DateOfBirth:"", Gender:"", Citizenship:"", Race:"",Checksum:"",IdNumber:""
+            IsValid:false,DateOfBirth:"000000", Gender:"0000", Citizenship:"0", Race:"0",Checksum:"0",IdNumber:"0000000000000"
         });
     }
     addToDatabase();
