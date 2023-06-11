a05f20fe0dc498a81de546676591126dbd862c07
ClearanceC137 <clearancetshepiso@gmail.com>
2023-06-11 17:48:24 +0200

    added loading spinner image and component

---
+++ src/Components/Loader/Loader.js
@@ -0,0 +1,12 @@
+/*
+    DisplayId buy time while other components render
+*/
+function Loader() {
+
+    return (
+        <div className="display-cn">
+           <img src="'../../../public/Loading_icon.gif'" alt="spinner"> </img>
+        </div>
+    );
+}
+export default Loader;
